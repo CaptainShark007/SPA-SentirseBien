3370e0ab064b0806db65b682af18e4b2535db74a
CaptainShark007 <hugo.lautaro.brocal@gmail.com>
2025-04-21 01:26:21 -0300

    añadiendo utilidad global de delay y tipos globales

---
+++ src/types/api.d.ts
@@ -0,0 +1,13 @@
+interface ApiReponse {
+  status: string;
+  message: string;
+}
+
+export interface ApiDataResponse<T> extends ApiReponse {
+  data: T;
+}
+
+export interface ApiParameters<T> {
+  id: number;
+  dataSend: T;
+}
